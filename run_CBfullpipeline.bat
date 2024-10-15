f6f380f535d5567b38ecee4ec30b7e31528fee9a
langes2 <98927488+langes2@users.noreply.github.com>
2024-10-15 09:42:53 -0700

    Add files via upload

---
+++ run_CBfullpipeline.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Public\Documents\ChargeBreakdownPipeline\CBcall\bin\Release\net8.0\win-x64\publish\CBcall.exe" > "C:\Users\Public\Documents\ChargeBreakdownPipeline\CleaningPipelineLogs\get.log" 2>&1
+"C:\Users\Public\Documents\ChargeBreakdownPipeline\run_CBcleaningpipeline.bat"
